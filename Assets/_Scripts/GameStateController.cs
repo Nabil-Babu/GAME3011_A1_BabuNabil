de253d8d95bb154d54bc679bad6b08ac6ae97d44
Nabil Babu <nabil.babu20@gmail.com>
2021-02-04 13:47:56 -0500

    Added GameStateController, placed Basic UI, Setup Game State functionality and connecting elements to the UI

---
+++ Assets/_Scripts/GameStateController.cs
@@ -0,0 +1,88 @@
+using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+using UnityEngine.UI; 
+
+public class GameStateController : MonoBehaviour
+{
+    [SerializeField] int totalGatheredResource = 0;
+    private static GameStateController _instance;
+
+    public static GameStateController Instance
+    {
+        get
+        {
+            if (_instance == null)
+            {
+                _instance = FindObjectOfType<GameStateController>(); 
+            }
+            return _instance;
+        }
+    }
+    private bool _gameEnabled;
+    
+    public bool scanningMode;
+    public bool extractionMode = true;
+    public int scanAttempts = 6;
+    public int extractionAttempts = 3;
+    public GameObject miniGameWindow;
+    public TMP_Text messageTextUI;
+    public TMP_Text resourceTextUI;
+
+    private string _messageOutput;
+
+    public void ToggleMiniGame()
+    {
+        _gameEnabled = !_gameEnabled;
+        miniGameWindow.SetActive(_gameEnabled);
+    }
+
+    public void EnableScanMode()
+    {
+        scanningMode = true;
+        extractionMode = false;
+        _messageOutput = "Enabled Scanning Mode \n" +
+                        "Attempts Left: "+scanAttempts.ToString();
+        UpdateMessageUI();
+    }
+
+    public void EnableExtractionMode()
+    {
+        extractionMode = true;
+        scanningMode = false;
+        _messageOutput = "Enabled Extraction Mode \n" +
+                         "Attempts Left: "+extractionAttempts.ToString();
+        UpdateMessageUI();
+    }
+    
+    public void ReduceScanAttempt()
+    {
+        scanAttempts--;
+        if (scanAttempts <= 0) scanAttempts = 0;
+        _messageOutput = "Scanning Area... \n" +
+                         "Attempts Left: "+scanAttempts.ToString();
+        UpdateMessageUI();
+    }
+
+    public void ReduceExtractionAttempt()
+    {
+        extractionAttempts--;
+        if (extractionAttempts <= 0) extractionAttempts = 0;
+        _messageOutput = "Extracting Node... \n" +
+                         "Attempts Left: "+extractionAttempts.ToString();
+        UpdateMessageUI();
+    }
+
+    public void AddResourcesToTotal(int amount)
+    {
+        totalGatheredResource += amount;
+        resourceTextUI.text = totalGatheredResource.ToString();
+        _messageOutput = "Adding Resources... \n";
+        UpdateMessageUI();
+    }
+    void UpdateMessageUI()
+    {
+        messageTextUI.text = _messageOutput;
+    }
+}
